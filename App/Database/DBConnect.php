e0b1b8e081ad3fa47929f0d3a2086ad411463830
HoneyDev312 <honeydev312@gmail.com>
2026-02-26 10:17:15 +0100

    remove connection ids

--- App/Database/DBConnect.php
+++ App/Database/DBConnect.php
@@ -4,13 +4,10 @@ declare(strict_types=1);
 
 namespace App\Database;
 
+require_once('config.php');
+
 class DBConnect
 {
-    const MYSQL_HOST = '127.0.0.1';
-    const MYSQL_PORT = 3306;
-    const MYSQL_NAME = 'adress_book_manager';
-    const MYSQL_USER = 'root';
-    const MYSQL_PASSWORD = '';
 
     public ?\PDO $database = null;
 
@@ -19,9 +16,9 @@ class DBConnect
 
         if ($this->database === null) {
             $this->database = new \PDO(
-                sprintf('mysql:host=%s;dbname=%s;port=%s;charset=utf8', self::MYSQL_HOST, self::MYSQL_NAME, self::MYSQL_PORT),
-                self::MYSQL_USER,
-                self::MYSQL_PASSWORD,
+                sprintf('mysql:host=%s;dbname=%s;port=%s;charset=utf8', MYSQL_HOST, MYSQL_NAME, MYSQL_PORT),
+                MYSQL_USER,
+                MYSQL_PASSWORD,
                 [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION],
             );
         }
